c324f3e4c77e7a1f1f29e5e84462209944a0c740
Ashis Pavan <ashispavan@live.com>
2018-11-04 17:03:04 +0530

    Added randomstoicquotesapi to generate random quotes;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import uniqueRandomArray from 'unique-random-array';
+
+const URL = 'https://randomstoicquotesapi.herokuapp.com/api/v1/quotes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public quote : any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getRandomQuote().subscribe((data) => {
+       let quoteItem = uniqueRandomArray(data.data);
+       this.quote = quoteItem().attributes.text;
+    })
+  }
+
+  getRandomQuote() : any {
+    return this.http.get(URL);
+  }
+
 }
